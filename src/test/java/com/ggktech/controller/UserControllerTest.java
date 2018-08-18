1776d32fe93a5d4957824f48e62fe0414641e42a
Ubuntu <ubuntu@ip-10-0-0-202.eu-west-1.compute.internal>
2018-08-18 10:23:35 +0000

    Remove pact tests from provider

--- src/test/java/com/ggktech/controller/UserControllerTest.java
+++ src/test/java/com/ggktech/controller/UserControllerTest.java
@@ -8,40 +8,40 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import com.ggktech.AppConfiguration;
 
-import au.com.dius.pact.provider.junit.PactRunner;
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.State;
-import au.com.dius.pact.provider.junit.VerificationReports;
-import au.com.dius.pact.provider.junit.loader.PactBroker;
-import au.com.dius.pact.provider.junit.loader.PactFolder;
-import au.com.dius.pact.provider.junit.target.HttpTarget;
-import au.com.dius.pact.provider.junit.target.Target;
-import au.com.dius.pact.provider.junit.target.TestTarget;
+//import au.com.dius.pact.provider.junit.PactRunner;
+//import au.com.dius.pact.provider.junit.Provider;
+//import au.com.dius.pact.provider.junit.State;
+//import au.com.dius.pact.provider.junit.VerificationReports;
+//import au.com.dius.pact.provider.junit.loader.PactBroker;
+//import au.com.dius.pact.provider.junit.loader.PactFolder;
+//import au.com.dius.pact.provider.junit.target.HttpTarget;
+//import au.com.dius.pact.provider.junit.target.Target;
+//import au.com.dius.pact.provider.junit.target.TestTarget;
 
-@RunWith(PactRunner.class)
-@Provider("pact_provider")
-@PactBroker(host="54.207.30.143", port = "8081")
+//@RunWith(PactRunner.class)
+//@Provider("pact_provider")
+//@PactBroker(host="54.207.30.143", port = "8081")
 //@PactFolder("/home/ubuntu/PactServices/Consumer/Consumer/target/pacts")
-@VerificationReports({ "console", "markdown" })
+//@VerificationReports({ "console", "markdown" })
 public class UserControllerTest {
 
-	@TestTarget
-	public final Target target = new HttpTarget(8080);
+	//@TestTarget
+	//public final Target target = new HttpTarget(8080);
 	
-	private static ConfigurableApplicationContext context;
+	//private static ConfigurableApplicationContext context;
 
-	@BeforeClass
-	public static void startService() {
-		context = SpringApplication.run(AppConfiguration.class);
-	}
+	//@BeforeClass
+	//public static void startService() {
+	//	context = SpringApplication.run(AppConfiguration.class);
+	//}
 
-	@State("test state")
-	public void testState() {
-		System.out.println("in test state.....");
-	}
+	//@State("test state")
+	//public void testState() {
+	//	System.out.println("in test state.....");
+	//}
 
-	@AfterClass
-	public static void stopService() {
-		context.stop();
-	}
+	//@AfterClass
+	//public static void stopService() {
+	//	context.stop();
+	//}
 }
